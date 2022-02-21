13be66a181d5c15c0f0c14fc46d71d50c91104a0
dom2code <dom2code@gmail.com>
2022-02-21 20:12:47 +0000

    changes

---
+++ Website/__init__.py
@@ -0,0 +1,8 @@
+# from email.mime import application
+# from flask import Flask
+
+# def create_app():
+#     application = Flask(__name__)
+#     application.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
+#     return application
+
--- Website/application.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-
-def create_app():
-    application = Flask(__name__)
-    application.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
-
-    return application
