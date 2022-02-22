98e66c2e675650c74f730007723cf59038a8e170
dom2code <dom2code@gmail.com>
2022-02-22 23:05:30 +0000

    trying

--- Website/__init__.py
+++ Website/__init__.py
@@ -1,18 +1,18 @@
-from email.mime import application
+# from email.mime import application
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from os import path
-from flask_login import LoginManager
+# from flask_sqlalchemy import SQLAlchemy
+# from os import path
+# from flask_login import LoginManager
 
-db = SQLAlchemy()
-DB_NAME = "database.db"
+# db = SQLAlchemy()
+# DB_NAME = "database.db"
 
 
 def create_app():
     application = Flask(__name__)
     application.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
-    application.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    db.init_application(application)
+    # application.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    # db.init_application(application)
 
     from .views import views
     from .auth import auth
@@ -20,22 +20,22 @@ def create_app():
     application.register_blueprint(views, url_prefix='/')
     application.register_blueprint(auth, url_prefix='/')
 
-    from .models import User
+    # from .models import User
 
-    create_database(application)
+    # create_database(application)
 
-    login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
-    login_manager.init_application(application)
+    # login_manager = LoginManager()
+    # login_manager.login_view = 'auth.login'
+    # login_manager.init_application(application)
 
-    @login_manager.user_loader
-    def load_user(id):
-        return User.query.get(int(id))
+    # @login_manager.user_loader
+    # def load_user(id):
+    #     return User.query.get(int(id))
 
     return application
 
 
-def create_database(application):
-    if not path.exists('website/' + DB_NAME):
-        db.create_all(application=application)
-        print('Created Database!')
+# def create_database(application):
+#     if not path.exists('website/' + DB_NAME):
+#         db.create_all(application=application)
+#         print('Created Database!')
--- Website/views.py
+++ Website/views.py
@@ -1,25 +1,25 @@
 from flask import Blueprint, render_template, request, flash, jsonify
-from flask_login import login_required, current_user
-from . import db
-import json
+# from flask_login import login_required, current_user
+# from . import db
+# import json
 
 views = Blueprint('views', __name__)
 
 
-@views.route('/', methods=['GET', 'POST'])
-@login_required
+@views.route('/')
+# @login_required
 def home():
-    return render_template("home.html", user=current_user)
+    return render_template("home.html")
 
 
-# @views.route('/delete-note', methods=['POST'])
-# def delete_note():
-#     note = json.loads(request.data)
-#     noteId = note['noteId']
-#     note = Note.query.get(noteId)
-#     if note:
-#         if note.user_id == current_user.id:
-#             db.session.delete(note)
-#             db.session.commit()
+# # @views.route('/delete-note', methods=['POST'])
+# # def delete_note():
+# #     note = json.loads(request.data)
+# #     noteId = note['noteId']
+# #     note = Note.query.get(noteId)
+# #     if note:
+# #         if note.user_id == current_user.id:
+# #             db.session.delete(note)
+# #             db.session.commit()
 
-#     return jsonify({})
+# #     return jsonify({})
