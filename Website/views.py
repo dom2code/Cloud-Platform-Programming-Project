9968a127560eedecad72005b5db0c5d2f0ff2de9
dom2code <dom2code@gmail.com>
2022-02-22 21:33:06 +0000

    try1

--- Website/views.py
+++ Website/views.py
@@ -1,7 +1,26 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash, jsonify
+from flask_login import login_required, current_user
+from .models import Note
+from . import db
+import json
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+
+@views.route('/', methods=['GET', 'POST'])
+@login_required
 def home():
-    return "<h1>Test</h1>"
+    return render_template("home.html", user=current_user)
+
+
+# @views.route('/delete-note', methods=['POST'])
+# def delete_note():
+#     note = json.loads(request.data)
+#     noteId = note['noteId']
+#     note = Note.query.get(noteId)
+#     if note:
+#         if note.user_id == current_user.id:
+#             db.session.delete(note)
+#             db.session.commit()
+
+#     return jsonify({})
