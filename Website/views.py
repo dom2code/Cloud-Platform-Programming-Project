da331423bf4550458cf2302ba625201e6bf4e13f
dom2code <dom2code@gmail.com>
2022-03-09 17:58:16 +0000

    homecommit

--- Website/views.py
+++ Website/views.py
@@ -7,19 +7,9 @@ views = Blueprint('views', __name__)
 
 
 @views.route('/')
-# @login_required
 def home():
     return render_template("home.html")
 
+ 
 
-# # @views.route('/delete-note', methods=['POST'])
-# # def delete_note():
-# #     note = json.loads(request.data)
-# #     noteId = note['noteId']
-# #     note = Note.query.get(noteId)
-# #     if note:
-# #         if note.user_id == current_user.id:
-# #             db.session.delete(note)
-# #             db.session.commit()
 
-# #     return jsonify({})
